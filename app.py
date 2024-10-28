af93381353f3fdac8e8c60b029140c03d6de2dae
Kota Kawagoe <s2122126@cco.kanagawa-it.ac.jp>
2024-10-28 14:20:46 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+# app.py
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('text_change')
+def handle_text_change(data):
+    emit('update_text', data, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
